5f3927e02147dd920671631a72ad077133a7e956
he.wang <hljkdwh@163.com>
2018-09-13 18:04:44 +0800

    加入打包配置

--- src/main/java/com/melot/kk/springcloud/zuul/Application.java
+++ src/main/java/com/melot/kk/springcloud/zuul/Application.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
-public class ZuulApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZuulApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
