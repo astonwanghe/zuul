63305e33122b1504b5a05a808e381f80ca7cc986
he.wang <hljkdwh@163.com>
2018-09-14 10:11:04 +0800

    配置编码修改

--- src/main/java/com/melot/kk/springcloud/zuul/filter/AccessFilter.java
+++ src/main/java/com/melot/kk/springcloud/zuul/filter/AccessFilter.java
@@ -36,31 +36,31 @@ public class AccessFilter extends ZuulFilter {
 
         LOGGER.info("AccessFilter, request method:{}, url:{}", request.getMethod(), request.getRequestURL().toString());
 
-        String userIdStr = request.getParameter("userId");
-
-        JSONObject result = new JSONObject();
-
-        if (userIdStr == null) {
-            result.put("TagCode", "00000001");
-
-            LOGGER.warn("AccessFilter, check fail, userId is null.");
-            currentContext.setSendZuulResponse(false);
-            currentContext.setResponseBody(result.toJSONString());
-
-            return null;
-        }
-
-        String token = request.getParameter("token");
-
-        if (token == null || token.isEmpty()) {
-            result.put("TagCode", "00000002");
-
-            LOGGER.warn("AccessFilter, check fail, token is null.");
-            currentContext.setSendZuulResponse(false);
-            currentContext.setResponseBody(result.toJSONString());
-
-            return null;
-        }
+//        String userIdStr = request.getParameter("userId");
+//
+//        JSONObject result = new JSONObject();
+//
+//        if (userIdStr == null) {
+//            result.put("TagCode", "00000001");
+//
+//            LOGGER.warn("AccessFilter, check fail, userId is null.");
+//            currentContext.setSendZuulResponse(false);
+//            currentContext.setResponseBody(result.toJSONString());
+//
+//            return null;
+//        }
+//
+//        String token = request.getParameter("token");
+//
+//        if (token == null || token.isEmpty()) {
+//            result.put("TagCode", "00000002");
+//
+//            LOGGER.warn("AccessFilter, check fail, token is null.");
+//            currentContext.setSendZuulResponse(false);
+//            currentContext.setResponseBody(result.toJSONString());
+//
+//            return null;
+//        }
 
         LOGGER.info("AccessFilter, check success!");
         return null;
